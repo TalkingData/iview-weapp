1a7eb58018fd05d7bc53e112f65c302b4d540109
梁灏 <admin@aresn.com>
2018-05-15 17:00:28 +0800

    add Panel component

---
+++ src/panel/index.js
@@ -0,0 +1,15 @@
+Component({
+    externalClasses: ['i-class'],
+
+    properties: {
+        title: {
+            type: String,
+            value: ''
+        },
+        // 内容区顶部是否取消边框
+        hideBorder: {
+            type: Boolean,
+            value: false
+        }
+    }
+});
