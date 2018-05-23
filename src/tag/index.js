3faf6b2f17967eca241136f1332033bfc61357dd
mo.duan <mo.duan@tendcloud.com>
2018-05-23 17:00:03 +0800

    add tag ui assembly

--- src/tag/index.js
+++ src/tag/index.js
@@ -1,4 +1,50 @@
 Component({
     externalClasses: ['i-class'],
-
-});
+    properties : {
+        //close button show or not;
+        closable : {
+            type : Boolean,
+            value : false
+        },
+        //slot name
+        name : {
+            type : String,
+            value : ''
+        },
+        //can click or not click
+        checkable : {
+            type : Boolean,
+            value : false
+        },
+        //is current choose
+        checked : {
+            type : Boolean,
+            value : true
+        },
+        //background and color setting
+        color : {
+            type : String,
+            value : 'default'
+        },
+        //control fill or not
+        type : {
+            type : String,
+            value : 'dot'
+        } 
+    },
+    data : {
+        clickChecked : false
+    },
+    methods : {
+        tapTag(){
+            const data = this.data;
+            if( data.checkable ){
+                const checked = data.checked ? false : true;
+                this.triggerEvent('onchange',{
+                    name : data.name || '',
+                    checked : checked
+                });
+            }
+        }
+    }
+})
