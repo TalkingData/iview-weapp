c1653d1b6d0352d2c5e9e8e67fa521b8993a024a
小朋友 <ytstrive@sina.com>
2018-05-23 18:20:08 +0800

    add Badge  component

--- src/badge/index.js
+++ src/badge/index.js
@@ -1,4 +1,56 @@
-Component({
-    externalClasses: ['i-class'],
+const prefixCls = 'i-badge';
 
+Component({
+    externalClasses: ['i-class', 'i-class-alone'],
+    properties: {
+        count: {
+            type: Number,
+            value: 0
+        },
+        overflowCount: {
+            type: Number,
+            value: 99
+        },
+        dot: {
+            type: Boolean,
+            value: false
+        },
+    },
+    data: {
+        classes: '',
+        dotClasses: '',
+        countClasses: '',
+        finalCount: 0
+    },
+    attached() {
+        this.initCls();
+    },
+    methods: {
+        initCls() {
+            this.classes();
+            this.dotClasses();
+            this.countClasses();
+            this.finalCount();
+        },
+        classes() {
+            this.setData({
+                classes: `${prefixCls}`
+            });
+        },
+        dotClasses() {
+            this.setData({
+                dotClasses: `${prefixCls}-dot`
+            });
+        },
+        countClasses() {
+            this.setData({
+                countClasses: `${prefixCls}-count`
+            });
+        },
+        finalCount() {
+            this.setData({
+                finalCount: Number(this.data.count) >= Number(this.data.overflowCount) ? `${this.data.overflowCount}+` : this.data.count
+            });
+        },
+    }
 });
