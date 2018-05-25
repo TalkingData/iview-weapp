667b6f2fe87af9bfdd5474421cce01d1d9dc1226
梁灏 <admin@aresn.com>
2018-05-25 11:05:20 +0800

    add Page component

--- examples/pages/page/index.js
+++ examples/pages/page/index.js
@@ -1,3 +1,17 @@
 Page({
-
+    data: {
+        current: 1
+    },
+    handleChange ({ detail }) {
+        const type = detail.type;
+        if (type === 'next') {
+            this.setData({
+                current: this.data.current + 1
+            });
+        } else if (type === 'prev') {
+            this.setData({
+                current: this.data.current - 1
+            });
+        }
+    }
 });
