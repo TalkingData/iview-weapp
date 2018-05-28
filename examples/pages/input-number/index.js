b6eabcae480bf098d29a292830ec9f4fd7956f01
梁灏 <admin@aresn.com>
2018-05-28 16:45:11 +0800

    add InputNumber

--- examples/pages/input-number/index.js
+++ examples/pages/input-number/index.js
@@ -1,3 +1,18 @@
 Page({
+    data: {
+        value1: 1,
+        value2: 0.1
+    },
 
+    handleChange1 ({ detail }) {
+        this.setData({
+            value1: detail.value
+        })
+    },
+
+    handleChange2 ({ detail }) {
+        this.setData({
+            value2: detail.value
+        })
+    }
 });
