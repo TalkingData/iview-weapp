80e407eece6ebbcc9ddf2da6a8d7aa6f233ebaff
mo.duan <mo.duan@tendcloud.com>
2018-05-28 14:42:08 +0800

    add switch component and example

--- examples/pages/switch/index.js
+++ examples/pages/switch/index.js
@@ -1,3 +1,12 @@
 Page({
-
+    data : {
+        switch1 : false
+    },
+    onChange(event){
+        const detail = event.detail;
+        this.setData({
+            'switch1' : detail.value
+        })
+        
+    }
 });
