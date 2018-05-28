4bca638f1e1b130149d8bc4b61484f9a28c074b3
小朋友 <ytstrive@sina.com>
2018-05-29 00:31:03 +0800

    add Checkbox component

--- examples/pages/checkbox/index.js
+++ examples/pages/checkbox/index.js
@@ -1,3 +1,45 @@
 Page({
+    data: {
+        fruit: [{
+            id: 1,
+            name: '香蕉',
+        }, {
+            id: 2,
+            name: '苹果'
+        }, {
+            id: 3,
+            name: '西瓜'
+        }, {
+            id: 4,
+            name: '葡萄',
+        }],
+        current: ['苹果', '葡萄'],
+        position: 'right',
+        animal: '熊猫',
+        checked: false,
+        disabled: false,
+    },
+    handleFruitChange({ detail = {} }) {
+        const index = this.data.current.indexOf(detail.title);
+        index === -1 ? this.data.current.push(detail.title) : this.data.current.splice(index, 1);
+        this.setData({
+            current: this.data.current
+        });
+    },
+    handleClick() {
+        this.setData({
+            position: this.data.position.indexOf('left') !== -1 ? 'right' : 'left',
+        });
+    },
+    handleDisabled() {
+        this.setData({
+            disabled: !this.data.disabled
+        });
+    },
+    handleAnimalChange({ detail = {} }) {
+        this.setData({
+            checked: detail.current
+        });
+    },
 
 });
--- src/checkbox/index.js
+++ src/checkbox/index.js
@@ -1,4 +1,80 @@
+const prefixCls = 'i-checkbox';
+
 Component({
     externalClasses: ['i-class'],
-
+    relations: {
+        '../checkbox-group/index': {
+            type: 'parent'
+        }
+    },
+    properties: {
+        title: {
+            type: String,
+            value: ''
+        },
+        checked: {
+            type: Boolean,
+            value: false
+        },
+        disabled: {
+            type: Boolean,
+            value: false
+        },
+        color: {
+            type: String,
+            value: '#2d8cf0'
+        },
+        iconPosition: {
+            type: String,
+            value: 'left',
+            observer: 'setIconPosition'
+        }
+    },
+    data: {
+        checked: true,
+        wrapClasses: [],
+        iconType: 'circle',
+        iconColor: '',
+        iconCls: `${prefixCls}-icon-left`,
+        iconDisabled: `${prefixCls}-icon-disabled`,
+    },
+    attached() {
+        this.initCls();
+        this.setIcon();
+        this.setIconPosition();
+    },
+    methods: {
+        initCls() {
+            this.setData({
+                wrapClasses: this.wrapClasses(),
+            })
+        },
+        wrapClasses() {
+            return [
+                `${prefixCls}`,
+            ];
+        },
+        changeCurrent(current) {
+            this.setData({ checked: current });
+            this.setIcon();
+        },
+        checkboxChange() {
+            if (this.data.disabled) return;
+            const item = { current: !this.data.checked, title: this.data.title };
+            const parent = this.getRelationNodes('../checkbox-group/index')[0];
+            parent ? parent.emitEvent(item) : this.triggerEvent('change', item);
+            this.setIcon();
+        },
+        setIcon() {
+            this.setData({
+                iconType: this.data.checked ? 'success' : 'circle',
+                iconColor: this.data.checked ? this.data.color : '',
+            });
+        },
+        setIconPosition() {
+            this.setData({
+                iconCls: this.data.iconPosition.indexOf('left') !== -1 ? `${prefixCls}-icon-left` : `${prefixCls}-icon-right`,
+            });
+        }
+    }
 });
