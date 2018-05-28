8600f49e7304b517f398971ee1a404eafaf865a4
mo.duan <mo.duan@tendcloud.com>
2018-05-28 18:09:29 +0800

    add rate component and example

--- examples/pages/rate/index.js
+++ examples/pages/rate/index.js
@@ -1,3 +1,18 @@
 Page({
-
+    data : {
+        starIndex1 : 0,
+        starIndex2 : 0
+    },
+    onChange1(e){
+        const index = e.detail.index;
+        this.setData({
+            'starIndex1' : index
+        })
+    },
+    onChange2(e){
+        const index = e.detail.index;
+        this.setData({
+            'starIndex2' : index
+        })
+    }
 });
