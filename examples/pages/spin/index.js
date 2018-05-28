cbfca3cec9544ef930c8150de04bc914515d98d4
梁灏 <admin@aresn.com>
2018-05-28 15:22:47 +0800

    update Spin

--- examples/pages/spin/index.js
+++ examples/pages/spin/index.js
@@ -1,3 +1,13 @@
 Page({
-
+    data: {
+        spinShow: true,
+        switch: false
+    },
+    onSwitchChange ({ detail }) {
+        const value = detail.value;
+        this.setData({
+            switch: value,
+            spinShow: !value
+        });
+    }
 });
