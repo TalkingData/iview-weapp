005368d4405b251a83405a57a477acdfeb0e4f44
梁灏 <admin@aresn.com>
2018-05-29 15:16:34 +0800

    update Input

--- examples/pages/input/index.js
+++ examples/pages/input/index.js
@@ -3,6 +3,9 @@ Page({
         value1: '',
         value2: '',
         value3: '',
-        value4: '输入框已禁用'
+        value4: '输入框已禁用',
+        value5: '',
+        value6: '',
+        value7: ''
     }
 });
