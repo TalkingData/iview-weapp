f7eed45b0ad8ed107ff006ee6bfead69c4259af3
梁灏 <admin@aresn.com>
2018-05-30 16:59:46 +0800

    add Toast component

--- src/toast/index.js
+++ src/toast/index.js
@@ -1,4 +1,44 @@
+const default_data = {
+    visible: false,
+    content: '',
+    icon: '',
+    image: '',
+    duration: 2,
+    mask: true,
+    type: 'default', // default || success || warning || error || loading
+};
+
+let timmer = null;
+
 Component({
     externalClasses: ['i-class'],
 
+    data: {
+        ...default_data
+    },
+
+    methods: {
+        handleShow (options) {
+            this.setData({
+                ...options,
+                visible: true
+            });
+
+            const duration = this.data.duration * 1000;
+
+            if (timmer) clearTimeout(timmer);
+            if (duration !== 0) {
+                timmer = setTimeout(() => {
+                    this.handleHide();
+                    timmer = null;
+                }, duration);
+            }
+        },
+
+        handleHide () {
+            this.setData({
+                ...default_data
+            });
+        }
+    }
 });
