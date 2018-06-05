5ee00e379e69393faa7d04057b693512fc7fca03
梁灏 <admin@aresn.com>
2018-06-05 17:00:08 +0800

    update List demo

--- examples/pages/list/index.js
+++ examples/pages/list/index.js
@@ -1,3 +1,12 @@
 Page({
+    data : {
+        switch1 : true
+    },
+    onChange(event){
+        const detail = event.detail;
+        this.setData({
+            'switch1' : detail.value
+        })
 
+    }
 });
