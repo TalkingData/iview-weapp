939094983db38faad9ef3107e684361bae6aabff
梁灏 <admin@aresn.com>
2018-06-05 16:25:55 +0800

    update index share

--- examples/pages/index/index.js
+++ examples/pages/index/index.js
@@ -1,3 +1,8 @@
 Page({
-
+    onShareAppMessage() {
+        return {
+            title: 'iView Weapp',
+            imageUrl: 'https://file.iviewui.com/iview-weapp-logo.png'
+        };
+    },
 });
