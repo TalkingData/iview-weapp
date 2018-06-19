09a1c6562b668bfb30bb922451299ef0947a2b3e
mo.duan <mo.duan@tendcloud.com>
2018-06-19 15:38:42 +0800

    add divider component

---
+++ src/divider/index.js
@@ -0,0 +1,25 @@
+Component({
+    externalClasses: ['i-class'],
+    properties: {
+        content: {
+            type: String,
+            value: ''
+        },
+        height : {
+            type: Number,
+            value: 48
+        },
+        color : {
+            type : String,
+            value : '#333'
+        },
+        lineColor : {
+            type : String,
+            value : '#f7f7f7'
+        },
+        size : {
+            type: String,
+            value: 12
+        }
+    }
+});
