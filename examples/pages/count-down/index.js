a93730ec0d878dbadc7a53b67728b0856bb6a70c
lijiao.li <lijiao.li@tendcloud.com>
2018-06-23 15:18:16 +0800

    增加倒计时组件

---
+++ examples/pages/count-down/index.js
@@ -0,0 +1,21 @@
+Page({
+  data: {
+    targetTime: 0,
+    targetTime1: 0,
+    myFormat: ['时', '分', '秒'],
+    myFormat1: ['天', '时', '分', '秒'],
+    status: '进行中...'
+  },
+  onLoad() {
+    this.setData({
+      targetTime: new Date().getTime() + 6430000,
+      targetTime1: new Date().getTime() + 86430000,
+      targetTime2: new Date().getTime() + 10000
+    });
+  },
+  myLinsterner(e) {
+    this.setData({
+      status: '结束'
+    });
+  }
+});
