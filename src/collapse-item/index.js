aff5d4968130b187626f3736e29aa162527cd868
梁灏 <admin@aresn.com>
2018-07-09 11:23:51 +0800

    update Collapse

--- src/collapse-item/index.js
+++ src/collapse-item/index.js
@@ -1,41 +1,46 @@
 Component({
-  relations: {
-    '../collapse/index': {
-      type: 'parent', 
-      linked: function(target) {
-        const options = {
-          accordion: target.data.accordion
+    externalClasses: ['i-class-content', 'i-class-title'],
+
+    relations: {
+        '../collapse/index': {
+            type: 'parent',
+            linked: function (target) {
+                const options = {
+                    accordion: target.data.accordion
+                }
+                if (target.data.name === this.data.name) {
+                    options.showContent = 'i-collapse-item-show-content';
+                }
+                this.setData(options);
+            }
         }
-        if (target.data.name === this.data.name) {
-          options.showContent = 'show-content';
-        }
-        this.setData(options);
-      }
-    }
-  },
-  externalClasses: ['i-class-content','i-class-title'],
-  options: {
-    multipleSlots: true 
-  },
-  properties: {
-    title: String,
-    name: String
-  },
-  data: {
-    showContent: '',
-    accordion: false
-  },
-  methods: {
-    trigger(e) {
-      const data = this.data;
-      if (data.accordion) {
-        this.triggerEvent('collapse', {name: data.name}, {composed: true, bubbles: true});
-      } else {
-        this.setData({
-          showContent: data.showContent ? '' : 'show-content' 
-        });
-      }
     },
-  }
+
+    properties: {
+        title: String,
+        name: String
+    },
+
+    data: {
+        showContent: '',
+        accordion: false
+    },
+
+    options: {
+        multipleSlots: true
+    },
+
+    methods: {
+        trigger(e) {
+            const data = this.data;
+            if (data.accordion) {
+                this.triggerEvent('collapse', {name: data.name}, {composed: true, bubbles: true});
+            } else {
+                this.setData({
+                    showContent: data.showContent ? '' : 'i-collapse-item-show-content'
+                });
+            }
+        },
+    }
 });
 
--- src/collapse/index.js
+++ src/collapse/index.js
@@ -1,29 +1,31 @@
 Component({
-  relations: {
-    '../collapse-item/index': {
-      type: 'child'
-    }
-  },
-  properties: {
-    name: String,
-    accordion: Boolean
-  },
-  methods: {
-    clickfn(e) {
-      const params = e.detail;
-      const allList = this.getRelationNodes('../collapse-item/index');
-      allList.forEach((item) => {
-        if (params.name === item.data.name) {
-          item.setData({
-            showContent: 'show-content'
-          });
-        } else {
-          item.setData({
-            showContent: ''
-          });
+    externalClasses: ['i-class'],
+
+    relations: {
+        '../collapse-item/index': {
+            type: 'child'
         }
-      });
     },
-  }
+    properties: {
+        name: String,
+        accordion: Boolean
+    },
+    methods: {
+        clickfn(e) {
+            const params = e.detail;
+            const allList = this.getRelationNodes('../collapse-item/index');
+            allList.forEach((item) => {
+                if (params.name === item.data.name) {
+                    item.setData({
+                        showContent: 'i-collapse-item-show-content'
+                    });
+                } else {
+                    item.setData({
+                        showContent: ''
+                    });
+                }
+            });
+        },
+    }
 });
 
